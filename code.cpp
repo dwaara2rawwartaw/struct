6686740030831c031b1fd6281d941e52d620296a
dwaara2rawwartaw <kaasp45@gmail.com>
2025-05-05 09:10:10 +0000

    code 2

--- code.cpp
+++ code.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-struct PERSON 
-{
-    int age;
-    long ss;
-    float weight;
-    char name[25];
-} family_member;
-
 struct CELL 
 { 
     unsigned short character  : 8;
@@ -19,23 +11,40 @@ struct CELL
 } screen[25][80];
 
 int main() {
-
-    struct PERSON sister;
-    PERSON brother;    
-    sister.age = 13;
-    brother.age = 7;
-    cout << "sister.age = " << sister.age << '\n';
-    cout << "brother.age = " << brother.age << '\n';
     CELL apelsin;
-    apelsin.character = 'g';
-    apelsin.intensity = 0;
-    apelsin.blink = 1;
-    apelsin.foreground = 3;
-    CELL *papelsin(&apelsin);
-    apelsin.intensity = 1;
-    apelsin.blink=0;
-    cout << "apelsin.character = " << apelsin.character << endl;
-    cout << "apelsin.intensity = " << apelsin.intensity << endl;
-    cout << "apelsin.blink = " << apelsin.blink << endl;
+    cout << "putb apelsin'a = "     << &apelsin           << endl;
+    apelsin.character   = 'g';
+    apelsin.intensity   = 0;
+    apelsin.blink       = 1;
+    apelsin.foreground  = 3;
+    
+    cout << "apelsin.character = "  << apelsin.character  << endl;
+    cout << "apelsin.intensity = "  << apelsin.intensity  << endl;
+    cout << "apelsin.blink = "      << apelsin.blink      << endl;
     cout << "apelsin.foreground = " << apelsin.foreground << endl;
+    cout << " " << endl;
+
+    unsigned char* bytePtr = reinterpret_cast<unsigned char*>(&apelsin); // Обращение непосредственно к apelsin. Меняет местами байты.
+    *bytePtr |= (1<<(3));
+
+    cout << "After Izmeneniya: " << endl;
+    cout << "apelsin.character = "  << apelsin.character  << endl;
+    cout << "apelsin.intensity = "  << apelsin.intensity  << endl;
+    cout << "apelsin.blink = "      << apelsin.blink      << endl;
+    cout << "apelsin.foreground = " << apelsin.foreground << endl;
+    cout << " " << endl;
+
+    cout << "Poly4enie baitov v pamyati: " << endl; // Перевод значений в шестнадцатиричную систему
+    unsigned char* bytes = reinterpret_cast<unsigned char*>(&apelsin);
+    cout << std::hex << static_cast<int>(bytes[0]) << endl;
+    cout << std::hex << static_cast<int>(bytes[1]) << endl;
+    cout << " " << endl;
+
+    cout << "After eshe odni izmeneniya: " << endl;
+    cout << "apelsin.character = "  << apelsin.character  << endl;
+    cout << "apelsin.intensity = "  << apelsin.intensity  << endl;
+    cout << "apelsin.blink = "      << apelsin.blink      << endl;
+    cout << "apelsin.foreground = " << apelsin.foreground << endl;
+
+    return 0;
 }
